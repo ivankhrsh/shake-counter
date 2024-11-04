971fbcd84f91dc578901c7c61b82f10531c5530b
ivankhrsh <ivan.khoroshylov.dev@gmail.com>
2024-11-05 01:56:33 +0200

    add shake animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  extends: "eslint-config-molindo/tsconfig.json",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,6 +12,25 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      animation: {
+        shake: "shake 0.82s cubic-bezier(.36,.07,.19,.97) both",
+      },
+      keyframes: {
+        shake: {
+          "10%, 90%": {
+            transform: "translate3d(0, -8px, 0)",
+          },
+          "20%, 80%": {
+            transform: "translate3d(0, 16px, 0)",
+          },
+          "30%, 50%, 70%": {
+            transform: "translate3d(0, -32px, 0)",
+          },
+          "40%, 60%": {
+            transform: "translate3d(0, 32px, 0)",
+          },
+        },
       },
     },
   },
