fd97d66dc9a065f3bc2b9acfbf0ce860423ef4c9
ivankhrsh <ivan.khoroshylov.dev@gmail.com>
2024-11-05 01:58:33 +0200

    add footer

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
+      <a
+        className="hover:border-1 group flex items-center gap-2 rounded-sm p-2 hover:text-blue-500 hover:underline hover:underline-offset-4"
+        href="https://github.com/ivankhrsh"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <Image
+          alt="GitHub icon"
+          aria-hidden
+          height={18}
+          src="https://nextjs.org/icons/github.svg"
+          width={18}
+        />
+        GitHub
+      </a>
+
+      <a
+        className="hover:border-1 group flex items-center gap-2 rounded-sm p-2 hover:text-blue-500 hover:underline hover:underline-offset-4"
+        href="https://t.me/nullOn"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Telegram
+      </a>
+    </footer>
+  );
+}
