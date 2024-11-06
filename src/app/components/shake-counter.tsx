e2ca0c09e55fa40004601e8230291434673e9cae
ivankhrsh <ivan.khoroshylov.dev@gmail.com>
2024-11-06 03:30:32 +0200

    update import

--- src/app/components/shake-counter.tsx
+++ src/app/components/shake-counter.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import useDeviceMotion from "../hooks/useDeviceMotion";
+import useDeviceMotion from "../hooks/use-device-motion";
 import { cn } from "@/lib/utils";
 
 export default function ShakeCounter() {
