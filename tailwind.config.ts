9a422ce0e2505fc18dc68f7edc718ae10dfe5277
ivankhrsh <ivan.khoroshylov.dev@gmail.com>
2024-11-06 03:14:05 +0200

    add new animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,9 @@ const config: Config = {
       },
       animation: {
         shake: "shake 0.82s cubic-bezier(.36,.07,.19,.97) both",
+        glow: "glow 1.5s ease-in-out infinite",
+        shakeGlow: "shakeGlow 1.5s ease-in-out infinite",
+        reset: "reset 0.5s ease-in-out",
       },
       keyframes: {
         shake: {
@@ -30,6 +33,42 @@ const config: Config = {
           "40%, 60%": {
             transform: "translate3d(0, 32px, 0)",
           },
+        },
+        glow: {
+          "0%, 100%": {
+            boxShadow: `0 0 10px var(--glow-color-primary), 0 0 20px var(--glow-color-secondary)`,
+          },
+          "50%": {
+            boxShadow: `0 0 20px var(--glow-color-intense), 0 0 40px var(--glow-color-primary)`,
+          },
+        },
+        shakeGlow: {
+          "0%, 100%": {
+            transform: "translate3d(0, 0, 0)",
+            boxShadow:
+              "0 0 10px var(--glow-color-primary), 0 0 20px var(--glow-color-secondary)",
+          },
+          "10%, 90%": {
+            transform: "translate3d(0, -8px, 0)",
+          },
+          "20%, 80%": {
+            transform: "translate3d(0, 16px, 0)",
+          },
+          "30%, 50%, 70%": {
+            transform: "translate3d(0, -32px, 0)",
+          },
+          "40%, 60%": {
+            transform: "translate3d(0, 32px, 0)",
+          },
+          "50%": {
+            boxShadow:
+              "0 0 20px var(--glow-color-intense), 0 0 40px var(--glow-color-primary)",
+          },
+        },
+        reset: {
+          "0%": { opacity: "1", transform: "scale(1)" },
+          "50%": { opacity: "0", transform: "scale(0.8)" },
+          "100%": { opacity: "1", transform: "scale(1)" },
         },
       },
     },
